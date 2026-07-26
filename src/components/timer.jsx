3288b6ced7aa60a23c3f1a746af78e66931d7acc
Satya Sarathi Mohanty <satyasarathi2005@gmail.com>
2026-07-26 17:02:47 +0530

    Update timer.jsx

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -1,16 +1,22 @@
 import { useEffect, useState } from "react";
 
-function Timer({ seconds = 120 }) {
+function Timer({ seconds = 90, currentQuestion }) {
   const [time, setTime] = useState(seconds);
 
+  // Reset timer 
+  useEffect(() => {
+    setTime(seconds);
+  }, [currentQuestion, seconds]);
+
+  // Countdown
   useEffect(() => {
     if (time <= 0) return;
 
-    const timer = setInterval(() => {
+    const timer = setTimeout(() => {
       setTime((prev) => prev - 1);
     }, 1000);
 
-    return () => clearInterval(timer);
+    return () => clearTimeout(timer);
   }, [time]);
 
   const minutes = String(Math.floor(time / 60)).padStart(2, "0");
